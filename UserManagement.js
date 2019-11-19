623cdb603e94e44bf2d24b2f673ff51ab12b4d90
Juanp44 <56651738+Juanp44@users.noreply.github.com>
2019-11-18 19:01:54 -0600

    Update UserManagement.js
    Juan Pablo Ramos Robles 715592 
    
    Funciones de Admin, información del admin, show users, delete users, edit users

--- UserManagement.js
+++ UserManagement.js
@@ -3,16 +3,19 @@
 let linkDatos = "https://localhost:3000/users"
 
 
-//MOSTRAR TODOS LOS USUARIOS EN LA VISTA DE ADMIN
+//********** VISTA DE ADMIN ***********/
 
+
+//PARA MOSTRAR LOS USUARIOS EN LA VISTA DE ADMING
+
+//EVENTO QUE SE ACTIVA AL DAR CLICK EN EL BOTON DE MOSTRAR USUARIOS
 let usersBoton = document.getElementById("usersBtn")
 usersBoton.addEventListener("click", function (event) {
     event.preventDefault()
 
-    let urlShowUsers = "http://localhost:3000/users"
+    let urlShowUsers = "http://localhost:3000/users" //LINK PARA ACCEDER A LOS USUARIOS
 
-
-
+    //COMO REQUIERIMOS ACCEDES A USUARIOS DEL DB.JSON, USAMOS UN GET
     fetch(urlShowUsers, {
             method: 'GET',
             headers: {
@@ -21,11 +24,16 @@ usersBoton.addEventListener("click", function (event) {
         })
         .then(response => response.json())
         .then(function (JSON) {
-
+            //METO LOS USUARIOS EN UN ARREGLO PARA IR ACCEDIENDO UNO A UNO
             let usuariosArreglo = new Array(JSON.length)
 
+            //HAGO UN CICLO PARA ITERAR DE INICIO A FIN DE LOS USUARIOS
             for (let i = 0; i < JSON.length; i++) {
-
+                /*
+                PARA INSERTAR LOS USUARIOS FUE UN POCO TRABAJOSO PERO SE LOGRÓ, YA TENÍA UNA TABLA VACÍA EN EL HTML ASÍ QUE LO QUE HICE FUE
+                CREAR MÁS RENGLONES POR CADA USUARIO Y COLUMNAS PARA SUS CARACTERÍSTICAS DE CADA UNO E IRLAS INSERTANTO CON INSERTADJACENTHRML
+                ASÍ POR CADA ITERACIÓN VAS AÑADIENDO DE UNO EN UNO LOS USUARIOS 
+                */
                 usuariosArreglo[i] =
                     `
                 <tr>
@@ -45,29 +53,36 @@ usersBoton.addEventListener("click", function (event) {
             }
             let usuariosId = document.getElementById("usuariosId")
             for (let i = 0; i < JSON.length; i++) {
+                //AÑADES A LA TABLA QUE YA TENEMOS EN EL HTML LA INFORMACION DE CADA UNO 
                 usuariosId.insertAdjacentHTML('afterend', usuariosArreglo[i])
             }
 
         })
 })
 
+
 //******** BORRAR USUARIO **********/
+
+/*
+ESTOS BOTONES SE MANDAN LLAMAR EL LA SECCION PASADA, AL CREAR LA TABLA CADA UNO DE ELLOS TIENE BOTON DE DELETE Y EDIT
+ASÍ QUE POR ESO NO ESTÁ EN UN EVENTO DETERMINADO
+*/
 function userRemove(event) {
     event.preventDefault()
 
-    let actualUser =
-        event.currentTarget.parentElement.
-    previousElementSibling.innerText
+    //TENEMOS QUE ACCEDER AL ID PARA IDENTIFIAR A LOS USUARIOS, CON ESTO RECORRO LA INFORMACION HASTA LLEGAR A ELLOS
+    let actualUser = event.currentTarget.parentElement.previousElementSibling.innerText
 
     console.log(actualUser)
 
     let borrarSi = document.getElementById('btnRemove')
-
+    //EVENTO QUE SE HACE CUANDO CLICK EN EL BOTON DE BORRAR
     borrarSi.addEventListener("click", function (event) {
         event.preventDefault()
 
-        let linkBorrar = "http://localhost:3000/users/" + actualUser
+        let linkBorrar = "http://localhost:3000/users/" + actualUser//LINK A USAR PARA LLEGAR A ESE USUARIO
 
+        //FETCH PARA BORRAR EL USUARIO
         fetch(linkBorrar, {
                 //AHORA USO DELETE YA QUE LO VOY A BORRAR
                 method: 'DELETE',
@@ -83,11 +98,16 @@ function userRemove(event) {
     })
 }
 
+
+
 //*********EDITAR USUARIO *********/
 
+
+//IGUAL QUE PARA EDITAR USSUARIOS, ESTA FUNCION ES LLAMADA EN LOS BOTONES CREADOS EN LA TABLA DE MOSTRAR USUARIOS
 function editData(event) {
     event.preventDefault()
 
+    //PASAMOS LA INFORMACION DE LOS INPUTS A VARIABBLES 
     let editName = document.getElementById("editName").value
     let editEmail = document.getElementById("editEmail").value
     let editContra1 = document.getElementById("editContra1").value
@@ -96,7 +116,7 @@ function editData(event) {
 
 
     //PARA LLENAR EL MODAL ANTES Y MODIFIQUES LOS DATOS 
-
+    //REFERENCIAMOS LA INFORMACION DE CADA USIARIO 
     let actualEmail =
         event.currentTarget.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.innerText
     console.log(actualEmail)
@@ -107,37 +127,35 @@ function editData(event) {
         event.currentTarget.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.innerText
     console.log(actualName)
 
-
+    //YA QUE TIENES LOS VALORES, LOS INSERTAS EN EL INPUT PARA QUE APAREZCAN UNA VEZ QUE ABRAS EL MODAL COMO PREDETERMINADOS
     document.getElementById("editName").value = actualName
     document.getElementById("editEmail").value = actualEmail
     document.getElementById("editContra1").value = actualPassword
     document.getElementById("editContra2").value = actualPassword
 
-    // let oldData = {
-    //     nombre: editName,
-    //     email: editEmail,
-    //     contraseña: editCotntra2
-    // }
-    let actualUser =
-        event.currentTarget.parentElement.
-    previousElementSibling.innerText
+    //CONSEGUIMOS EL ID DE CADA USUARIO A EDITAR
+    let actualUser =event.currentTarget.parentElement.previousElementSibling.innerText
     console.log(actualUser)
 
+    //EVENTO QUE SE EJECUTA AL HACER CLICK EN EL BOTON DE EDITAR DE CADA USUARIO
     let updateBtn = document.getElementById("updateBtn")
     updateBtn.addEventListener("click", function (event) {
-        event.preventDefault()
+        event.preventDefault()//PARA QUE NO SE REFRESQUE LA PAGINA
 
+        //PASO A UN OBJETO LA INFORMACION DEL USUARIO
         let newData = {
             nombre: document.getElementById("editName").value,
             email: document.getElementById("editEmail").value,
             contraseña: document.getElementById("editContra2").value
         }
 
+        //PASO EL OBJETO A UN JSON
         let nuevosDatos = JSON.stringify(newData)
 
 
-        let linkEditar = "http://localhost:3000/users/"+actualUser
+        let linkEditar = "http://localhost:3000/users/" + actualUser//LINK PARA ACCEDER AL USUARIO
 
+        //FETCH PARA HACER LA EDICION DEL USUARIO
         fetch(linkEditar, {
                 //USO PUT YA QUE AHORA YO SOY EL QUE METE NUEVA INFO
                 method: 'PUT',
