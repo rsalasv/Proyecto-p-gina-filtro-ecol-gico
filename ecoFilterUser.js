d0ec94cb2ae5d76cd06d45b4f5eafc09405fcf35
TheOutl4nder <37419487+TheOutl4nder@users.noreply.github.com>
2019-11-18 22:21:29 -0600

    Add files via upload

--- ecoFilterUser.js
+++ ecoFilterUser.js
@@ -4,15 +4,19 @@
 let urlCarrito = "http://localhost:3000/carrito"
 let urlProductos = "http://localhost:3000/products"
 let cart={products:[],size:0}
-let cartIndex=0;
+let cartIndex;
+if(localStorage.getItem("index")===null)
+    cartIndex=0;
+else
+    cartIndex=localStorage.getItem("index");
 
 function addFilter(event){
     let cartItem={id:""}
     let item=event.target;
     if(item===document.getElementById("product1"))
-        cartItem.id=1;
+        cartItem.id=0;
     else if (item===document.getElementById("product2"))
-        cartItem.id=2;
+        cartItem.id=1;
     //POR EL MOMENTO ES UN CARRITO UNIVERSAL YA QUE NO TENEMOS UN TOKEN PARA IDENTIFICAR AL USUARIO, LA INFO DEL CARRITO SE
     //ALMACENA EN LOCAL-STORAGE EL ID DEL ITEM PARA USARLO EN EL CARRITO Y AHÍ HACE EL GET DEL SERVIDOR
    localStorage.setItem("cartItem"+cartIndex,cartItem.id);
