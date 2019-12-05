a8a5018bc915b7ed201d8476a6042239b4ca34c0
Juanp44 <56651738+Juanp44@users.noreply.github.com>
2019-12-05 16:05:41 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,39 +2,82 @@
 //Juan Pablo Ramos Robles 
 
 const express = require('express')
+const jwt = require('jsonwebtoken')
 const app = express()
 const {
     usuariosModelo,
     crearUsuario,
     deleteUser,
-    getUser
+    getUser,
+    showUsers
 } = require('./Usuarios')
 const port = 3000
-
 app.use(express.json())
-
 app.listen(port, () => console.log(`Conectado a http://localhost:${port}`))
 
 
-app.route('/api/EcoFiltro')
+app.route('/users')
+
     .get(async (req, res) => {
-        res.send("Funcionó el GET")
+        let users = await showUsers()
+        console.log(users)
+        return res.status(200).send(users)
     })
 
     .post(async (req, res) => {
         console.log(req.body);
         let usuario = usuariosModelo(req.body)
-        await crearUsuario(usuario)
-        res.status(200).send(req.body)
+        
+        jwt.sign({usuario}, 'secretKey', (err,token) =>{
+            res.json({
+                message: 'Se creo el token',
+                token: token//checa esta parteeeee
+            })
+        })
+         await crearUsuario(usuario)
+         res.status(200).send(req.body)
     })
 
-app.route('/api/EcoFiltro:id')
+app.route('/users:id')
     .delete((req, res) => {
-        console.log(req.params.email);
+        console.log(req.params.id);
         let resultado = deleteUser(req.params.id)
         if (resultado == 1) {
-            return res.status(200).send("Se eliminó el usuario")
+            return res.status(400).send("No se eliminó el usuario")
         } else {
-            return res.status(400).send('No se eliminó al usuario correctamente')
+            return res.status(200).send(' Se eliminó al usuario correctamente')
         }
     })
+
+    .get(async (req, res) => {
+        res.send("Funcionó el GET2")
+    })
+    
+app.route('/api/Login')
+    .post(verifyToken,(req,res) =>{
+        jwt.verify(req.token, 'secretKey', (err, authData) =>{
+            if(err){
+                return res.sendStatus(403)
+            }
+            else{
+                res.json({
+                    message: 'Created post',
+                    authData
+                })
+            }
+        })
+    })
+
+function verifyToken(req,res,next){
+    const bearerHeader = req.headers['authorization'];
+    if(typeof bearerHeader !== undefined){
+        const bearer = bearerHeader.split(' ')
+        const bearerToken = bearer[1]
+        req.token = bearerToken
+        next();
+    }
+    else{
+        req.sendStatus(403)
+    }
+}
+
